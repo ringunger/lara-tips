0ab91f6b27f069e209beb865c7b6f63c9b0775ef
Ringle <ringunger@gmail.com>
2023-02-23 01:37:00 +0300

    Endable serching tips`

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Alpine from 'alpinejs'
 import { LaraTips } from "./src/LaraTips.js";
+import DOMPurify from "./node_modules/dompurify/dist/purify.es"
 window.Alpine = Alpine;
-
+window.marked = import("./node_modules/marked/marked.min");
 Alpine.data('lara_tips', () => ({
     sections: [],
     tips: [],
     activeSection: null,
     activeTip: null,
+    tipSearch: '',
     lt: new LaraTips(),
     readSections() {
         this.sections = this.lt.getSections();
@@ -22,5 +24,14 @@ Alpine.data('lara_tips', () => ({
         this.activeTip = tip;
         // TODO load the tip to the
     },
+
+    closeTip() {
+        this.activeTip = null;
+    },
+    parseMarkDown(markdown) {
+        return DOMPurify.sanitize(marked.parse(
+            markdown.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/,"")
+        ));
+    }
 }))
 Alpine.start();
